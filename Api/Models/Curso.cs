1a3833c0440d7595f03c1ebf6c45a5989e42e59e
Kumegawa <lucaskumegawa222@gmail.com>
2025-04-25 16:08:43 -0300

    validações com DataAnnotations nos modelos

--- Api/Models/Curso.cs
+++ Api/Models/Curso.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Curso
 {
     public int Id { get; set; }
--- Api/Models/Materia.cs
+++ Api/Models/Materia.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 public class Materia
 {
     public int Id { get; set; }
